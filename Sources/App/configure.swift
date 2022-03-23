c36477a4a22f19363e3a569e3fbbf7cf3121a32b
Ben Kreeger <ben@kree.gr>
2022-03-22 21:50:40 -0500

    Cleanup a few files

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,12 +2,7 @@ import Vapor
 import Fluent
 import FluentSQLiteDriver
 
-// configures your application
 public func configure(_ app: Application) throws {
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
     try routes(app)
 }

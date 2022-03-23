41e462f6a572514d6c6ded6551e80a052bbf9ead
Ben Kreeger <ben@kree.gr>
2022-03-22 21:43:52 -0500

    Hook up Fluent with SQlite

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,7 +1,10 @@
 import Vapor
+import Fluent
+import FluentSQLiteDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
